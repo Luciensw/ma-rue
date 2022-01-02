e4dbe8976931b85c5e1d819fb57758e0401af24b
Luciensw <77001976+Luciensw@users.noreply.github.com>
2022-01-02 17:06:06 +0100

    Add files via upload

---
+++ modules/rue_finale.py
@@ -0,0 +1,25 @@
+# Dépendances
+
+from sol import sol
+from immeuble import *
+
+# Définitions
+
+
+def rue_finale(canvas):
+    
+    # Affichage de ma rue
+    affiche(canvas)
+    
+    
+    # Dessin des immeubles
+    for k in range(4):
+        immeuble(rue.width/5*(k+1))
+    
+        
+    # Dessin du sol de la rue
+    sol()
+    
+# Tests
+if __name__ == '__main__':
+    rue_finale(rue)
